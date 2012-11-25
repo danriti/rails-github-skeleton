89065d502448450bbbdc61fefdf05d3ba3aa0568
Dan Riti <dmriti@gmail.com>
2012-11-25 18:35:03 -0500

    Generate rails files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsGithubSkeleton::Application.config.secret_token = 'e54c4a8cb8c092eb12f15c26b56c7838d03b2a2739eff5384fd3b642a02465b405013ec3392d1116aeeeb380b65c8b7eff73258791a317fbad27e19d05dfb411'
