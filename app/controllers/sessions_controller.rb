213736b3579e7f31194af712aa8a1a0c7ae5d873
Dan Riti <driti@gmail.com>
2013-04-15 11:47:38 -0400

    Add endpoint to get logged in user token.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,28 @@
 class SessionsController < ApplicationController
   def create
     #raise request.env["omniauth.auth"].to_yaml
-    user = User.from_omniauth(env["omniauth.auth"])
+    auth = env["omniauth.auth"]
+    user = User.from_omniauth(auth)
     session[:user_id] = user.id
+    session[:user_token] = auth["credentials"]
     redirect_to root_url, notice: "Signed in!"
   end
 
+  def get
+    cred = session[:user_token]
+    if cred
+      return render :json => {'status' => 'ok',
+                              'data' => cred}
+    else
+      return render :json => {'status' => 'error',
+                              'data' => nil}
+    end
+  end
+
   def destroy
+    # Destroy session variables.
     session[:user_id] = nil
+    session[:user_token] = nil
     redirect_to root_url, notice: "Signed out!"
   end
 end
